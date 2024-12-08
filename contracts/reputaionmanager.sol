8ec50b77fe37a54792b2bafb700617b5f91ff6b7
Anurag <141274029+Anu-De@users.noreply.github.com>
2024-12-09 00:38:37 +0530

    Update reputaionmanager.sol

--- contracts/reputaionmanager.sol
+++ contracts/reputaionmanager.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ReputationManager {
+    mapping(address => uint256) public reputationScore;
+
+    function updateReputation(address _creator, bool _successful) external {
+        if (_successful) {
+            reputationScore[_creator] += 10;
+        } else {
+            reputationScore[_creator] -= 5;
+        }
+    }
+
+    function getReputation(address _creator) external view returns (uint256) {
+        return reputationScore[_creator];
+    }
+}
 
